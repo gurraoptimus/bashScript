5017843a336f7b9cefd8327f86bf88ca53b48c81
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-05-16 22:38:39 +0200

    Add files via upload

--- Backup.sh
+++ Backup.sh
@@ -3,4 +3,5 @@
 source=/home//Dokument/source
 dest=/home//Dokument/destination
 
-for file in $(find $source -printf)
+for file in $(find $source -printf "%P\n") ; do
+	if [ -a $source/$file -nt $d ] ; then 
