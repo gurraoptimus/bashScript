e75d126594a10110d89c926afe481208d393e73e
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-05-16 23:27:07 +0200

    backup

--- Backup.sh
+++ Backup.sh
@@ -3,7 +3,8 @@ source=/home//Dokument/source
 dest=/home//Dokument/destination
 
 for file in $(find $source -printf "%P\n") ; do
-	if [ -a $source/$file -nt $dest/$file ] ; then
+	if [-a $dest/$file ] ; then
+	if [ $source/$file -nt $dest/$file ] ; then
 		echo "Newer file detected, copying..."
 		cp -r $source/$file $dest/$file
 		else
