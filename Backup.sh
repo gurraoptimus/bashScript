65e7faf3f10d1e9d17c4e994b852ffa6fcfd24b8
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-05-16 22:50:29 +0200

    Add files via upload

--- Backup.sh
+++ Backup.sh
@@ -4,4 +4,6 @@ source=/home//Dokument/source
 dest=/home//Dokument/destination
 
 for file in $(find $source -printf "%P\n") ; do
-	if [ -a $source/$file -nt $d ] ; then 
+	if [ -a $source/$file -nt $dest/$file ] ; then
+		echo "Newer file detected, copying..."
+		cp -r $source/
