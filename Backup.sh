708600f648b1e210bea60103a564878e6bef12a4
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-07-20 21:12:45 +0200

    add user

--- Backup.sh
+++ Backup.sh
@@ -1,6 +1,6 @@
-#!//
-source=/home//Dokument/source
-dest=/home//Dokument/destination
+#!/bin/bash
+source=/home/user/Dokument/source
+dest=/home/user/Dokument/destination
 
 for file in $(find $source -printf "%P\n") ; do
 	if [-a $dest/$file ] ; then
